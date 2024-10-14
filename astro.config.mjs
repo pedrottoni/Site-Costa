8e2a5fce9ce1f5d13603eeaac4c28aedd13b5ab3
pedrottoni <pedrottoni@gmail.com>
2024-10-14 20:29:33 -0300

    instalando o vercel no astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+// Import /static for a static site
+import vercelStatic from '@astrojs/vercel/static';
+ 
+export default defineConfig({
+  // Must be 'static' or 'hybrid'
+  output: 'static',
+  adapter: vercelStatic(),
+});
